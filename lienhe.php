78bb9286235433a24d23bc1acf996a7490d7554f
hailm <hai.lemanh.1310@gmail.com>
2025-05-06 10:13:23 +0700

    feat: page lienhe

--- lienhe.php
+++ lienhe.php
@@ -2,6 +2,97 @@
 <?php $isHome = false; ?>
 <?php $bodyClass = '' ?>
 <?php require "template-parts/layouts/header.php"; ?>
-
-
+<div class="uk-section-xsmall uk-background-norepeat uk-background-norepeat uk-background-center-center uk-background-blend-screen bgc-c3f5ff uk-background-cover" data-src="images/bg-breacrumd.webp" uk-img>
+    <div class="uk-container">
+        <h2 class="fz-24 barlow-semibold text-3161C2 uk-margin-remove">Liên hệ</h2>
+        <nav class="item-14px" aria-label="Breadcrumb">
+            <ul class="uk-breadcrumb uk-margin-remove">
+                <li><a href="#">Trang chủ</a></li>
+                <li><span aria-current="page">Liên hệ</span></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+<div class="uk-section-small">
+    <div class="uk-container">
+        <h1 class="uk-text-center uk-text-uppercase fz-24 text-F69C1F uk-margin-remove-bottom">liên hệ với chúng tôi</h1>
+        <div class="uk-text-center fz-20 barlow-medium item-6px">Để được hỗ trợ tư vấn về Dịch vụ VR360 hãy liên hệ với chúng tôi theo thông tin sau:</div>
+        <div class="uk-card uk-card-default uk-card-body item-38px rounded-7px contact__card">
+            <div class="uk-child-width-1-2@l" uk-grid>
+                <div>
+                    <h4 class="fz-20 barlow-medium text-3161C2 uk-text-uppercase">Công ty TNHH Đầu tư và phát triển công nghệ THT</h4>
+                    <address>
+                        <div class="uk-grid-small uk-grid" uk-grid>
+                            <div class="uk-width-auto">
+                                <div class="uk-cover-container">
+                                    <div class="uk-position-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="17" height="22" viewBox="0 0 17 22" fill="none">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M2.5835 8.49284C2.5835 4.94534 5.45266 2.07617 9.00016 2.07617C12.5477 2.07617 15.4168 4.94534 15.4168 8.49284C15.4168 12.3153 11.3652 17.5862 9.706 19.5937C9.33933 20.0337 8.67016 20.0337 8.3035 19.5937C6.63516 17.5862 2.5835 12.3153 2.5835 8.49284ZM6.7085 8.49284C6.7085 9.75784 7.73516 10.7845 9.00016 10.7845C10.2652 10.7845 11.2918 9.75784 11.2918 8.49284C11.2918 7.22784 10.2652 6.20117 9.00016 6.20117C7.73516 6.20117 6.7085 7.22784 6.7085 8.49284Z" fill="#484554"/>
+                                        </svg>
+                                    </div>
+                                    <canvas width="20" height="20"></canvas>
+                                </div>
+                            </div>
+                            <div class="uk-width-expand">
+                                <div>Tòa Elcom, ngõ 15 Duy Tân, phường Dịch Vọng Hậu, quận Cầu Giấy, Hà Nội</div>
+                            </div>
+                        </div>
+                        <div class="uk-grid-small uk-grid" uk-grid>
+                            <div class="uk-width-auto">
+                                <div class="uk-cover-container">
+                                    <div class="uk-position-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M13.9015 12.4818L16.0181 12.7235C16.8598 12.8235 17.4931 13.5318 17.4848 14.3735V15.8151C17.4848 16.7568 16.7015 17.5401 15.7598 17.4818C8.65147 17.0401 2.9598 11.3568 2.51814 4.24014C2.4598 3.29847 3.24314 2.51514 4.1848 2.51514H5.62647C6.47647 2.51514 7.1848 3.14847 7.2848 3.99014L7.52647 6.09014C7.5848 6.59847 7.4098 7.09847 7.05147 7.4568L5.5098 8.99847C6.7098 11.3651 8.64314 13.2901 11.0015 14.4901L12.5348 12.9568C12.8931 12.5985 13.3931 12.4235 13.9015 12.4818ZM10.7265 4.21514C13.3765 4.54847 15.4515 6.62347 15.7848 9.27347C15.8348 9.69847 16.1931 10.0068 16.6098 10.0068C16.6431 10.0068 16.6765 10.0068 16.7098 9.99847C17.1681 9.94014 17.4931 9.52347 17.4348 9.06514C17.0098 5.6568 14.3348 2.9818 10.9265 2.5568C10.4681 2.5068 10.0515 2.8318 10.0015 3.29014C9.94314 3.74847 10.2681 4.16514 10.7265 4.21514ZM11.0431 5.97347C10.6015 5.8568 10.1431 6.12347 10.0265 6.57347C9.9098 7.02347 10.1765 7.47347 10.6265 7.59014C11.5015 7.81514 12.1848 8.49847 12.4181 9.3818C12.5181 9.7568 12.8515 10.0068 13.2265 10.0068C13.2931 10.0068 13.3681 9.99847 13.4348 9.9818C13.8765 9.86514 14.1431 9.4068 14.0348 8.96514C13.6431 7.49014 12.5015 6.34847 11.0431 5.97347Z" fill="#484554"/>
+                                        </svg>
+                                    </div>
+                                    <canvas width="20" height="20"></canvas>
+                                </div>
+                            </div>
+                            <div class="uk-width-expand">
+                                <div>(+84) 987 15 6688</div>
+                            </div>
+                        </div>
+                        <div class="uk-grid-small uk-grid" uk-grid>
+                            <div class="uk-width-auto">
+                                <div class="uk-cover-container">
+                                    <div class="uk-position-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="19" height="20" viewBox="0 0 19 20" fill="none">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M2.33317 3.33325H15.6665C16.5832 3.33325 17.3332 4.08325 17.3332 4.99992V14.9999C17.3332 15.9166 16.5832 16.6666 15.6665 16.6666H2.33317C1.4165 16.6666 0.666504 15.9166 0.666504 14.9999V4.99992C0.666504 4.08325 1.4165 3.33325 2.33317 3.33325ZM9.88317 10.2833L15.3332 6.87492C15.5415 6.74159 15.6665 6.51659 15.6665 6.27492C15.6665 5.71659 15.0582 5.38325 14.5832 5.67492L8.99984 9.16659L3.4165 5.67492C2.9415 5.38325 2.33317 5.71659 2.33317 6.27492C2.33317 6.51659 2.45817 6.74159 2.6665 6.87492L8.1165 10.2833C8.65817 10.6249 9.3415 10.6249 9.88317 10.2833Z" fill="#484554"/>
+                                        </svg>
+                                    </div>
+                                    <canvas width="20" height="20"></canvas>
+                                </div>
+                            </div>
+                            <div class="uk-width-expand">
+                                <div>Anhtuan.cntht@gmail.com</div>
+                            </div>
+                        </div>
+                        <div class="uk-grid-small uk-grid" uk-grid>
+                            <div class="uk-width-auto">
+                                <div class="uk-cover-container">
+                                    <div class="uk-position-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
+                                            <path fill-rule="evenodd" clip-rule="evenodd" d="M1.75 10.5C1.75 5.67 5.66125 1.75 10.4913 1.75C15.33 1.75 19.25 5.67 19.25 10.5C19.25 15.33 15.33 19.25 10.4913 19.25C5.66125 19.25 1.75 15.33 1.75 10.5ZM13.9738 7H16.555C15.715 5.55625 14.3763 4.43625 12.7663 3.885C13.2913 4.85625 13.6938 5.90625 13.9738 7ZM10.5 3.535C11.2262 4.585 11.795 5.74875 12.1713 7H8.82875C9.205 5.74875 9.77375 4.585 10.5 3.535ZM3.5 10.5C3.5 11.1038 3.5875 11.69 3.7275 12.25H6.685C6.615 11.6725 6.5625 11.095 6.5625 10.5C6.5625 9.905 6.615 9.3275 6.685 8.75H3.7275C3.5875 9.31 3.5 9.89625 3.5 10.5ZM4.445 14H7.02625C7.30625 15.0938 7.70875 16.1438 8.23375 17.115C6.62375 16.5638 5.285 15.4525 4.445 14ZM4.445 7H7.02625C7.30625 5.90625 7.70875 4.85625 8.23375 3.885C6.62375 4.43625 5.285 5.5475 4.445 7ZM10.5 17.465C9.77375 16.415 9.205 15.2513 8.82875 14H12.1713C11.795 15.2513 11.2262 16.415 10.5 17.465ZM8.3125 10.5C8.3125 11.095 8.37375 11.6725 8.4525 12.25H12.5475C12.6263 11.6725 12.6875 11.095 12.6875 10.5C12.6875 9.905 12.6263 9.31875 12.5475 8.75H8.4525C8.37375 9.31875 8.3125 9.905 8.3125 10.5ZM12.7663 17.115C13.2913 16.1438 13.6938 15.0938 13.9738 14H16.555C15.715 15.4438 14.3763 16.5638 12.7663 17.115ZM14.4375 10.5C14.4375 11.095 14.385 11.6725 14.315 12.25H17.2725C17.4125 11.69 17.5 11.1038 17.5 10.5C17.5 9.89625 17.4125 9.31 17.2725 8.75H14.315C14.385 9.3275 14.4375 9.905 14.4375 10.5Z" fill="#484554"/>
+                                        </svg>
+                                    </div>
+                                    <canvas width="20" height="20"></canvas>
+                                </div>
+                            </div>
+                            <div class="uk-width-expand">
+                                <div>www.vr360tht.com.vn</div>
+                            </div>
+                        </div>
+                    </address>
+                </div>
+                <div>
+                    <div class="fz-16 barlow-bold">Bản đồ chỉ đường</div>
+                    <div class="item-6px">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d59735.54182262184!2d106.01758720000001!3d20.6503936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1zLA!5e0!3m2!1svi!2s!4v1746501101569!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" uk-responsive=""></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="uk-section-small uk-visible@l"></div>
 <?php require "template-parts/layouts/footer.php"; ?>
